a829b1e4507c60527e0b6b3a2015b22155a2cb57
Jabi <michal.jabi@gmail.com>
2025-12-01 15:39:52 +0100

    task(3): First component: Header.

--- auction-portal/src/App.jsx
+++ auction-portal/src/App.jsx
@@ -1,12 +1,10 @@
-// import "./App.css";
+import { Header } from "./Header.jsx";
 
 export function App() {
 
   return (
     <>
-      
+      <Header />
     </>
   );
 }
-
-export default App;
---
+++ auction-portal/src/Header.jsx
@@ -0,0 +1,8 @@
+export function Header() {
+  return (
+    <div className="p-2 p-sm-3 p-lg-5 bg-warning">
+      <h1>Auction Portal</h1>
+      <h6>kupuj - sprzedawaj - zarabiaj</h6>
+    </div>
+  );
+}
