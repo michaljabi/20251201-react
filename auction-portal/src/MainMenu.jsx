fff70c20f3f387e4426e9dd16178493150089b23
Jabi <michal.jabi@gmail.com>
2025-12-02 11:38:28 +0100

    task(7): Burger menu - works. Explain STATE

--- auction-portal/src/MainMenu.jsx
+++ auction-portal/src/MainMenu.jsx
@@ -1,10 +1,26 @@
+import { useState } from "react";
+
  export function MainMenu() {
+
+    // tak nie da rady - to musi być STAN!!!! bo zmienia się w czasie (po kliknięciu!)
+  // let isMenuOpen = true; 
+
+  // to jest STAN - zmiana w czasie dzięki setIsMenuOpen, konwencja, że zaczyna się od set.... nazwa.
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light px-3 mb-3">
-      <button className="navbar-toggler" type="button">
+      <button className="navbar-toggler" type="button" onClick={() => { 
+        // isMenuOpen = !isMenuOpen 
+       
+        // console.log(isMenuOpen)
+        // Aktualizacje stanu mogę uruchomić na 2 sposoby:
+        setIsMenuOpen(!isMenuOpen)
+        //setIsMenuOpen((iMO) => !iMO)
+        }}>
         <span className="navbar-toggler-icon"></span>
       </button>
-      <div className={"collapse navbar-collapse" + " show"}>
+      <div className={"collapse navbar-collapse" + ( isMenuOpen ? " show" : "" )}>
         <ul className="navbar-nav">
           <li className="nav-item">
             <a className="nav-link" href="auctions">
