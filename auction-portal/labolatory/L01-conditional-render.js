e90f696b32be6eef70f73b588e153d87db04d3ff
Jabi <michal.jabi@gmail.com>
2025-12-02 10:22:26 +0100

    explain: Conditional Rendering in JSX.

---
+++ auction-portal/labolatory/L01-conditional-render.js
@@ -0,0 +1,9 @@
+let user;
+
+console.log(user && "Zalogowany user to: " + user?.name);
+
+// logujemy usera:
+user = {name: 'Kasia'}
+
+
+console.log(user && "Zalogowany user to: " + user?.name);
--- auction-portal/src/Header.jsx
+++ auction-portal/src/Header.jsx
@@ -1,12 +1,15 @@
-const DEFAULT_USER = {name: 'nieznany'};
+const DEFAULT_USER = { name: "nieznany" };
 
-
-export function Header({appTitle = "Auction Portal", user}) {
+export function Header({ appTitle = "Auction Portal", user }) {
   return (
     <div className="p-2 p-sm-3 p-lg-5 bg-warning">
       <h1>{appTitle}</h1>
       <h6>kupuj - sprzedawaj - zarabiaj</h6>
-      Zalogowany user to: {user?.name}
+      {/* CONDITIONAL RENDERING: */}
+      {user && "Zalogowany user to: " + user?.name}
+      {/* wartości które nie renderują się w JSX ! */}
+      {undefined} {null} {''} {false} {true}
+      {/* powyżej w złożeniu user && - korzystamy z tej własnoci */}
     </div>
   );
 }
