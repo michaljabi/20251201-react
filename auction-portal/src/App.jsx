67a1e781754f600680cffaea17ee4f70d2b8f224
Jabi <michal.jabi@gmail.com>
2025-12-02 10:12:36 +0100

    task(5): Header props + ?. optional chaining + default value. You can send "Wathever" as a prop

--- auction-portal/src/App.jsx
+++ auction-portal/src/App.jsx
@@ -2,9 +2,14 @@ import { Header } from "./Header.jsx";
 
 export function App() {
 
+  const title = "Portal Aukcyjny?"
+
   return (
     <>
       <Header />
+      <Header appTitle="To" />
+      <Header appTitle="Portal Aukcyjny" />
+      <Header appTitle={title} user={{name: 'Michał'}} />
     </>
   );
 }
--- auction-portal/src/Header.jsx
+++ auction-portal/src/Header.jsx
@@ -1,8 +1,12 @@
-export function Header() {
+const DEFAULT_USER = {name: 'nieznany'};
+
+
+export function Header({appTitle = "Auction Portal", user}) {
   return (
     <div className="p-2 p-sm-3 p-lg-5 bg-warning">
-      <h1>Auction Portal</h1>
+      <h1>{appTitle}</h1>
       <h6>kupuj - sprzedawaj - zarabiaj</h6>
+      Zalogowany user to: {user?.name}
     </div>
   );
 }
