5de5ce7a655dd09bb3bb596e033cde2be89387b6
Jabi <michal.jabi@gmail.com>
2025-12-02 12:24:54 +0100

    task(8): Map to JSX - render collection of objects / data -> dynamically.

--- auction-portal/src/MainMenu.jsx
+++ auction-portal/src/MainMenu.jsx
@@ -1,14 +1,13 @@
 import { useState } from "react";
 
-
-
-
 export function MainMenu() {
   // tak nie da rady - to musi być STAN!!!! bo zmienia się w czasie (po kliknięciu!)
   // let isMenuOpen = true;
 
   // to jest STAN - zmiana w czasie dzięki setIsMenuOpen, konwencja, że zaczyna się od set.... nazwa.
-  const [isMenuOpen, setIsMenuOpen] = useState(Boolean(localStorage.getItem('isMenuOpen')));
+  const [isMenuOpen, setIsMenuOpen] = useState(
+    Boolean(localStorage.getItem("isMenuOpen"))
+  );
 
   function handleMenuToggle() {
     // isMenuOpen = !isMenuOpen
@@ -16,12 +15,19 @@ export function MainMenu() {
     // console.log(isMenuOpen)
     // Aktualizacje stanu mogę uruchomić na 2 sposoby:
     setIsMenuOpen(!isMenuOpen);
-    localStorage.setItem('isMenuOpen', !isMenuOpen ? true : '');
+    localStorage.setItem("isMenuOpen", !isMenuOpen ? true : "");
     //setIsMenuOpen((iMO) => !iMO)
-  };
+  }
 
   // najczęściej tak:
   // const handleMenuToggle2 = () => {};
+
+  const menuItems = [
+    { name: "Aukcje", link: "/auctions" },
+    // { name: "Aukcje", link: "/auctions" },
+    { name: "Promocje", link: "/promotions" },
+    { name: "Podpowiadamy", link: "/advices" },
+  ];
 
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light px-3 mb-3">
@@ -34,21 +40,34 @@ export function MainMenu() {
       </button>
       <div className={"collapse navbar-collapse" + (isMenuOpen ? " show" : "")}>
         <ul className="navbar-nav">
-          <li className="nav-item">
-            <a className="nav-link" href="auctions">
-              Aukcje
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="promotions">
-              Promocje
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="advices">
-              Podpowiadamy
-            </a>
-          </li>
+          {
+            /* key musi być: 1. Unikatowy 2. Nie-randomowy !!! 3. Nie może być to `index`  */
+            menuItems.map((mI) => (
+              <li className="nav-item" key={mI.name}>
+                <a className="nav-link" href={mI.link}>
+                  {mI.name}
+                </a>
+              </li>
+            ))
+          }
+          {/*
+            [
+              <li className="nav-item" key="1">
+                <a className="nav-link" href="auctions">
+                  Aukcje
+                </a>
+              </li>,
+              <li className="nav-item" key="2">
+                <a className="nav-link" href="promotions">
+                  Promocje
+                </a>
+              </li>,
+              <li className="nav-item" key="3">
+                <a className="nav-link" href="advices">
+                  Podpowiadamy
+                </a>
+              </li>
+            ]*/}
         </ul>
       </div>
     </nav>
