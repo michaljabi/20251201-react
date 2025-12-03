b5ec7e5f529d9cbd3b0c65f0fa5d359cd2ad118e
Jabi <michal.jabi@gmail.com>
2025-12-03 10:33:14 +0100

    explain: IMPORTANT: rules of hooks

--- auction-portal/src/common/NotFoundPage.jsx
+++ auction-portal/src/common/NotFoundPage.jsx
@@ -2,17 +2,22 @@ import { Link, useLocation, useNavigate } from "react-router";
 import { PageLayout } from "../shared/PageLayout.jsx";
 
 export function NotFoundPage() {
+
   const location = useLocation();
 
   // Przykład: nawigowanie z poziomu kodu.
   const navigate = useNavigate();
 
+  // https://react.dev/reference/rules
+  // https://react.dev/reference/rules/rules-of-hooks
+  if (location.pathname === "/easter-egg") {
+    return <>HAHAHA</>;
+  }
+
   return (
     <PageLayout title="404 Strona nie znaleziona">
       Strona <code>{location.pathname}</code> nie istnieje.
-      <div>
-        { JSON.stringify(location) }
-      </div>
+      <div>{JSON.stringify(location)}</div>
       <div className="mt-2">
         <Link className="btn btn-primary" to="/">
           Powrót do strony głównej
