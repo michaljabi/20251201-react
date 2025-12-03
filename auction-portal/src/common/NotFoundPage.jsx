4caf19c8884460a26a835ea9ebe7494cb4b491d3
Jabi <michal.jabi@gmail.com>
2025-12-03 10:25:21 +0100

    explain: useLocation + useNavigate hooks usage, examples

--- auction-portal/src/common/NotFoundPage.jsx
+++ auction-portal/src/common/NotFoundPage.jsx
@@ -1,19 +1,30 @@
-import { Link, useLocation } from "react-router";
+import { Link, useLocation, useNavigate } from "react-router";
 import { PageLayout } from "../shared/PageLayout.jsx";
 
 export function NotFoundPage() {
+  const location = useLocation();
 
-  const { pathname } = useLocation();
+  // Przykład: nawigowanie z poziomu kodu.
+  const navigate = useNavigate();
 
   return (
     <PageLayout title="404 Strona nie znaleziona">
-      Strona <code>{pathname}</code> nie istnieje.
+      Strona <code>{location.pathname}</code> nie istnieje.
+      <div>
+        { JSON.stringify(location) }
+      </div>
       <div className="mt-2">
         <Link className="btn btn-primary" to="/">
           Powrót do strony głównej
         </Link>
+        <hr />
+        <button
+          className="btn btn-secondary"
+          onClick={() => navigate("/advices")}
+        >
+          Zobacz nasze artykuły
+        </button>
       </div>
     </PageLayout>
   );
 }
-
