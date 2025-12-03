bde42ec2deb6782c247d01d330065f8e9d4126be
Jabi <michal.jabi@gmail.com>
2025-12-03 13:02:25 +0100

    task(14): load auctions on click from static `.getAll()` for now.

--- auction-portal/src/auctions/AuctionsPage.jsx
+++ auction-portal/src/auctions/AuctionsPage.jsx
@@ -1,23 +1,43 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { auctionService } from './auctions-service'
 import { AuctionCard } from "./AuctionCard.jsx";
 
 export function AuctionsPage() {
 
+  const [auctions, setAuctions] = useState([])
+
+  const loadAuctions = () => setAuctions(auctionService.getAll());
+
+  const addOneAuction = () => {
+    // setAuctions(auctionService.getAll())
+    const oneAuction = auctionService.getAll()[0];
+    // To jest MUTOWANIE danych - tak to nie zadziała!
+    // auctions.push(oneAuction);
+    // Nie możesz do settera wrzucić Referencji do tego samego obiektu!
+    // setAuctions(auctions);
+
+    // to jest ok:
+    setAuctions([...auctions, oneAuction])
+  }
+
   useEffect(() => {
     console.log("AuctionsPage osadzone na stronie, czas pobrać aukcje...");
   }, []);
 
-  const auction = auctionService.getAll()[0];
-
   return (
     <section>
       <h2>Lista naszych aukcji</h2>
+      <button className="btn btn-success" onClick={addOneAuction}>
+        Dodaj jedną aukcje
+      </button>
       <div className="row">
-        <div className="col-12 col-sm-6 col-md-4 col-lg-3"> 
-          { JSON.stringify(auction) }  
-          <AuctionCard auction={auction} addToCart={(a) => { console.log(a) }} />
-        </div>
+        {
+          auctions.map(a => (
+            <div className="col-12 col-sm-6 col-md-4 col-lg-3" key={a.id}> 
+              <AuctionCard auction={a} addToCart={(a) => { console.log(a) }} />
+            </div>
+          ))
+        }
       </div>
     </section>
   );
