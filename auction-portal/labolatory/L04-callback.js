3fd691780f8aa6c7e5cad4eb4770195219164f8b
Jabi <michal.jabi@gmail.com>
2025-12-03 11:54:53 +0100

    min-task(L04): collect callback.

---
+++ auction-portal/labolatory/L04-callback.js
@@ -0,0 +1,18 @@
+function giveMeMoney( callback ) {
+    callback(300)
+}
+
+
+// Obierz tutaj to 300:
+
+
+
+
+
+
+// Przykład "statyczny", z normalnym return
+function giveMeNumber() {
+    return 876;
+}
+
+console.log(giveMeNumber())
